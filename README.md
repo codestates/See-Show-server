3adde32acd40e5a4cd1729f9a9f4950f0eb478db
김태선 <75370296+rlaxotjs2745@users.noreply.github.com>
2021-06-07 23:12:25 +0900

    [server] open server, embody login, accessToken, parsing xml code with connecting api

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # im28project06-server
+
+6.7. 태선 / 서버 구축 및 login, accessToken 구현 완료, api연결 및 xml파싱 과정 일부 구현
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  login: require('./controllers/login'), // OAuth, 토큰
+  accessTokenRequest: require('./controllers/accessTokenRequest'), // 토큰 유효성 검사
+  refreshTokenRequest: require('./controllers/refreshTokenRequest'), // 토큰 재발급
+  myPage: require('./controllers/myPage'), // 회원정보 불러오기
+  show: require('./controllers/show'), // 공연 api에서 공연 정보 가져오기, review에 대한 요청 필요
+  location: require('./controllers/location'), // 위치 정보 받아 해당 위치로부터 일정 거리 안에 있는 공연 정보 불러오기
+  reviewGet: require('./controllers/reviewGet'), // show 요청 응답 후 요청을 받아 review 요청
+  reviewPost: require('./controllers/reviewPost') // show 요청 응답 후 요청을 받아 review 요청
+};
