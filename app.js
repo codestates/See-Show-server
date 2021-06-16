dc06171aceaf2f15cd67f1d307b069b0038a6153
Kim-Link <wildrays@gmail.com>
2021-06-16 20:56:49 +0900

    migration 재설정 seq 변경

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.get('/review', indexRouter.review.getRead); // 리뷰 리스트 불러오기
 app.post('/review', indexRouter.review.postDelete); // 리뷰 삭제
 
 app.get('/show', indexRouter.show.getList); // 공연 리스트 불러오기
-app.get('/show/detail', indexRouter.show.detailInfo); // 공연 상세정보
+app.post('/show/detail', indexRouter.show.detailInfo); // 공연 상세정보
 app.post('/show/posting', indexRouter.show.postMyShow); // 내 공연 등록
 
 app.post('/signUp', indexRouter.signUp.nat); // 자체 회원 가입
