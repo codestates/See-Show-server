3389250b78960fa5aef7cf98f5594adacd4ff3ad
i2sign <i2sign@icloud.com>
2021-08-24 21:56:09 +0900

    for utilfunction

--- routes/controllers/utilFunction.js
+++ routes/controllers/utilFunction.js
@@ -1,23 +1,50 @@
 require('dotenv').config();
 const axios = require('axios')
+import jwt from 'jsonwebtoken'
 const cookieParser = require('cookie-parser')
 cookieParser()
 
-utilFunctions = {
-  getToken: async(req,res)=> {
-      const authorization = req.headers["authorization"];
-      const { genre, area } = req.body;
+module.exports = {
+   //토큰유효성 검사
+   checkToken: async(req , res)=> {
+      const { authorization } = req.headers
       if (!authorization) {
-          res.status(404).send({data: null, message: 'invalid access token'})
+      return res.status(401).send({ message: 'Unauthorized' })
       }
-      let token = authorization.split(" ")[1];
-      let toggle = 0;
-      let verifytoken;
-      try {
-          verifytoken = await jwt.verify(token, process.env.ACCESS_SECRET);
-      } catch (err) {
-          token = await refreshTokenRequest(req)
-          toggle = 1;
+      const token = authorization.split(" ")[1];
+      //accesstoken디코딩
+      const data = jwt.verify(token, process.env.ACCESS_SECRET);
+      if (!data) {//accesstoken 이 없는 경우 -> 리프레스토큰 확인
+            //refreshtoken decoding
+            let decoding = jwt.verify(token, process.env.REFRESH_SECRET)
+            //리프레시토큰도 만료 된 경우,
+            if(!decoding ){ 
+                  // return res.status(400).send({ message: 'invalid refresh token, please log in again' })
+                  return;
+            }
+            //유효한 리프레시 토큰이 있는경우, 새로운 accesstoken 만들어준다
+            else { 
+                  let newAccessToken = jwt.sign(data, process.env.ACCESS_SECRET,{expiresIn:'1d'})
+                  return newAccessToken
+            }
       }
+  },
+      
+//   getToken: async(req,res) =>{
+//       const getAccessToken = (data) =>{
+//             return jwt.sign(data, processs.env.ACCESS_SECRET,{
+//                   expiresIn: "1s"
+//             })
+//       }
+//       const getRefreshToken = (data) => {
+//             return jwt.sign(data, process.env.REFRESH_SECRET,{
+//                   expiresIn:'1d'
+//             })
+//       }
+
+//   }
+  getUserInfo: asnyc(token)=>{
+      const userInfo= jwt.verify(token,process.env.ACCESS_SECRET)
+      return userInfo
   }
 }
